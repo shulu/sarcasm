85d204c64238eb0a4f44cc696685047456e0cc81
舒露 <shulubanggood>
2018-05-08 18:46:31 +0800

    ignore
    Signed-off-by: 舒露 <shulubanggood>

---
+++ conf/conf.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * -------------------------
+ *   config
+ * -------------------------
+ * User: shulu
+ * Date: 2018/5/3
+ * Time: 13:58
+ */
+
+
+return [
+
+	'index' => APP_PATH.'index',
+	
+	'backend' => APP_PATH.'admin',
+	
+	'mysql' => [
+		'host' => '',
+		'port' => '',
+		'user' => '',
+		'passwd' => ''
+	],
+	
+	'redis' => [
+		'host' => '',
+		'port' => '',
+		'auth' => ''
+	]
+];
